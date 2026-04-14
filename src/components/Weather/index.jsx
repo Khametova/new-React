e68dcb4c4165b696f39abe617101f27a9b336b23
Alina Khametova <alinakhametova1@gmail.com>
2026-04-14 20:58:56 +0300

    add "weather iP "

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const WEATHER_API =
+  "https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,sunrise,rain_sum";
 
 function Weather() {
-  return <div>Weather</div>;
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    fetch(WEATHER_API)
+      .then((response) => response.json())
+      .then((data) => setWeather(data))
+      .catch((e) => console.log("e", e));
+  }, []);
+
+  return (
+    <table>
+      <caption>weather</caption>
+      <thead>
+        <tr>
+          {weather?.daily?.time.map((t) => (
+            <th key={t}>{t}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {weather?.daily?.temperature_2m_max.map((tempmax) => {
+            <td key={index}>{tempmax}</td>;
+          })}
+        </tr>
+        <tr>
+          {weather?.daily?.temperature_2m_min.map((tempmin) => {
+            <td key={index}>{tempmin}</td>;
+          })}
+        </tr>
+        <tr>
+          {weather?.daily?.sunrise.map((sun) => {
+            <td key={index}>{sun}</td>;
+          })}
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default Weather;
